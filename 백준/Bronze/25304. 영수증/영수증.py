826c6088b970e23f1fd9bc8123249b4773236a27
hibobo98 <117331188+hibobo98@users.noreply.github.com>
2023-01-11 20:55:20 +0900

    [Bronze V] Title: 영수증, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -2,12 +2,11 @@ import sys
 X = sys.stdin.readline().rstrip()
 N = sys.stdin.readline().rstrip()
 l = []
+# a, b = sys.stdin.readline().rstrip().split()
 
+
+X = int(X)
 for i in range(int(N)):
     a, b = sys.stdin.readline().rstrip().split()
-    l.append(int(a)*int(b))
-s = sum(l)
-if s == int(X):
-    print('Yes')
-else:
-    print('No')
+    X -= int(a)*int(b)
+print("Yes" if X == 0 else "No")
