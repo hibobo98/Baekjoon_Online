cd858e037cfea45c695a234085ed635ce798bc9b
Bowon Kang <117331188+hibobo98@users.noreply.github.com>
2025-01-20 23:02:21 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0 
+    if n % 2 == 1:
+        for i in range(1, n+1):
+            if i % 2 == 1:
+                answer += i 
+            else:
+                pass
+    else:
+        for i in range(1, n+1):
+            if i % 2 == 0:
+                answer += i*i 
+            else:
+                pass
+      
+    return answer
