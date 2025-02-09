f45a895e4fd87c7bccb0c5e09f2343ace9b5fed7
Bowon Kang <117331188+hibobo98@users.noreply.github.com>
2025-02-09 15:34:42 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,23 +1,26 @@
-# 문제 3. 14501 퇴사
 def recur(idx, result):
     global answer
 
-    if idx > n:
-        if idx > n+1: return # 그만두는 날
+    # 퇴사 후 (상담을 더 이상 할 수 없는 상태)
+    if idx >= n:
         answer = max(answer, result)
         return
-    recur(idx+table[idx][0], result+table[idx][1])
-    recur(idx+1, result)
     
-n = int(input())
-table = [[] for _ in range(n+1)]
+    # 상담을 진행하는 경우 (idx + table[idx][0]일 후에 퇴사)
+    if idx + table[idx][0] <= n:
+        recur(idx + table[idx][0], result + table[idx][1])
 
+    # 상담을 진행하지 않는 경우 (다음 날로 넘어감)
+    recur(idx + 1, result)
+    
+n = int(input())  # 퇴사까지의 날짜 수
+table = [[] for _ in range(n+1)]  # table[0]부터 사용할 수 있도록 크기 설정
+
+# 상담 정보 입력
 for i in range(n):
     a, b = map(int, input().split())
-    table[i+1] = [a,b]
-# print(table) #[[], [12], [11]]
+    table[i] = [a, b]  # table[0]부터 사용
 
 answer = 0 
-recur(1,0)
-
+recur(0, 0)  # 0일부터 시작
 print(answer)
