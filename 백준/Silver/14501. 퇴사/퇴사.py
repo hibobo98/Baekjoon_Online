6f3ebb9530334b243c2523d73149e46384cb581c
Bowon Kang <117331188+hibobo98@users.noreply.github.com>
2025-02-09 15:04:51 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,23 @@
+# 문제 3. 14501 퇴사
+def recur(idx, result):
+    global answer
+
+    if idx > n:
+        if idx > n+1: return # 그만두는 날
+        answer = max(answer, result)
+        return
+    recur(idx+table[idx][0], result+table[idx][1])
+    recur(idx+1, result)
+    
+n = int(input())
+table = [[] for _ in range(n+1)]
+
+for i in range(n):
+    a, b = map(int, input().split())
+    table[i+1] = [a,b]
+# print(table) #[[], [12], [11]]
+
+answer = 0 
+recur(1,0)
+
+print(answer)
