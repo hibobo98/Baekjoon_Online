bd05140d956ce2c417c65796e8f25b7b7cbe929d
Bowon Kang <117331188+hibobo98@users.noreply.github.com>
2025-02-16 06:45:43 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,4 +1,8 @@
-def solution(start_num, end_num):
-    answer = [i for i in range(end_num, start_num+1)]
-    answer.sort(reverse=True)
-    return answer
+#def solution(start_num, end_num):
+#    answer = [i for i in range(end_num, start_num+1)]
+#    answer.sort(reverse=True)
+#    return answer
+
+
+def solution(start, end):
+    return list(range(start,end-1,-1))
